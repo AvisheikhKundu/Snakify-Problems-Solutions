f133398ca357eac84095b39f4d72bd9c853510bb
Avisheikh Kundu <avisheikh76@gmail.com>
2024-05-24 07:25:13 +0600

    add new code

--- 1 Input,Print and Numbers/Two timestamps.py
+++ 1 Input,Print and Numbers/Two timestamps.py
@@ -1,16 +1,7 @@
-hours1 = int(input("Enter the number of hours for the first timestamp: "))
-minutes1 = int(input("Enter the number of minutes for the first timestamp: "))
-seconds1 = int(input("Enter the number of seconds for the first timestamp: "))
-
-
-hours2 = int(input("Enter the number of hours for the second timestamp: "))
-minutes2 = int(input("Enter the number of minutes for the second timestamp: "))
-seconds2 = int(input("Enter the number of seconds for the second timestamp: "))
-
-total_seconds1 = hours1 * 3600 + minutes1 * 60 + seconds1
-total_seconds2 = hours2 * 3600 + minutes2 * 60 + seconds2
-
-# Calculate the difference in seconds
-difference_seconds = total_seconds2 - total_seconds1
-
-print(difference_seconds)
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+print(((h2*3600)+(m2*60)+(s2))-((h1*3600)+(m1*60)+(s1)))
